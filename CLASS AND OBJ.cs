11822d35b45e1dfe1ad6a32c324248b8b53ef027
Muhammad Irtaza Rizvi <irtazarizvi25@gmail.com>
2025-04-29 17:20:43 +0500

    Class N Object

---
+++ CLASS AND OBJ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace car
+{
+    class car1
+    {
+        public string brand;
+        public int model;
+        public float price;
+
+        public void detail()
+        {
+            Console.WriteLine("brand={0} model={1} price={2}", brand, model, price);
+        }
+    }
+
+    class c
+    {
+        static void Main(string[] args)
+        {
+            car1 c = new car1();
+            c.brand = "toyota";
+            c.model = 2022;
+            c.price = 350000.0F;
+
+            c.detail();
+        }
+    }
+}
