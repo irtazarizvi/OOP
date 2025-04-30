5222e0d713368ed0cafe63828ba5dc07edb433ca
Muhammad Irtaza Rizvi <irtazarizvi25@gmail.com>
2025-05-01 00:48:40 +0500

    Add files via upload

---
+++ book description.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace oop
+{
+    class book
+    {
+        public string bookname;
+        public string author;
+        public int pg;
+
+        public void book1()
+        {
+            Console.WriteLine("name of book is " + bookname + " by " + author + " and pages are " + pg);
+        }
+        
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            book b = new book();
+            b.bookname = "SHAYAD";
+            b.author = "Jaun Elia";
+            b.pg = 356;
+            b.book1();
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
