17f8bc9eeca70f49080355d164ae2a852f8f5748
Muhammad Irtaza Rizvi <irtazarizvi25@gmail.com>
2025-04-30 18:14:41 +0500

    Add files via upload

---
+++ area,perimeter of rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace rectangle
+{
+    class lw
+    {
+        public double length;
+        public double width;
+
+        public void area()
+        {
+            Console.WriteLine("area is :" + (length * width));
+        }
+        public void perimeter()
+        {
+            Console.WriteLine("perimeter is :" + (2 * (length + width)));
+        }
+    }
+    class program
+    {
+        static void Main(String[] args)
+        {
+            lw a = new lw();
+            a.width = 12.5;
+            a.length = 34.5;
+            a.area();
+            a.perimeter();
+            Console.ReadKey();
+
+        }
+    }
+}
