2ddb5f9fa641e2aabd19d8bd4cb448e341b0d037
Muhammad Irtaza Rizvi <irtazarizvi25@gmail.com>
2025-05-08 21:34:06 +0500

    Add files via upload

---
+++ inheritance basic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace inheritance
+{
+    class vehicle
+    {
+        public void start()
+        {
+            Console.WriteLine("vehicle is starting");
+        }
+        public void stop()
+        {
+            Console.WriteLine("vehicle is stopped");
+        }
+    }
+        class car : vehicle
+        {
+            public void drive()
+            {
+                Console.WriteLine("car is driving");
+            }
+        }
+            class bike : vehicle
+            {
+                public void ride()
+                {
+                    Console.WriteLine("bike  is riding");
+                }
+            }
+        
+            class program
+            {
+                static void Main(string[] args)
+                {
+                    car c = new car();
+                    c.start();
+                    c.drive();
+                    c.stop();
+                    bike b=new bike();
+                    b.start();
+                    b.ride();
+                    b.stop();
+
+                }
+            }   
+}
