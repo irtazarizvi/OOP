c957813d95087f1f01dbd015425bcf739119ecc3
Muhammad Irtaza Rizvi <irtazarizvi25@gmail.com>
2025-05-08 19:03:28 +0500

    Add files via upload

---
+++ encapsulation2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace encapsulation2
+{
+    class Student
+    {
+        private string name;
+        public void SetName(string n)
+        {
+            if (string.IsNullOrWhiteSpace(n))
+                Console.WriteLine("invalid name");
+            else
+                name = n;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void Display()
+        {
+            Console.WriteLine("name is " + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.SetName("irtaza rizvi");    
+            s.Display();
+            Console.ReadKey();
+       
+        
+        }
+    }
+                  
+                     
+}
